1da8faeb997f6b643b4bf04b6cab112449918108
Patryk Lesiak <patryklesiak.dev@gmail.com>
2024-11-30 04:05:03 +0100

    Intro to Audio

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource dialogueSource;
+    [SerializeField] private AudioSource[] soundSources;
+    void Start()
+    {
+        
+    }
+
+    void PlayMusic() {
+        
+    }
+}
