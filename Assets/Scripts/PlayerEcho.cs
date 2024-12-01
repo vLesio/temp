db595bcc9510933a84ddd3b13d77e4a9bb7001ec
Xenoneqq <realfatboimlg@gmail.com>
2024-12-01 13:31:47 +0100

    Added foot sound

--- Assets/Scripts/PlayerEcho.cs
+++ Assets/Scripts/PlayerEcho.cs
@@ -6,21 +6,34 @@ public class PlayerEcho : MonoBehaviour
     private EchoEffectFactory _echoEffectFactory;
     [SerializeField] private Echo footStepEcho;
     [SerializeField] private Echo snapEcho;
+    private AudioManager audioMgr;
+
     private AudioSource _audioSource;
     private void Start()
     {
+        HasAudioManager();
         _echoEffectFactory = transform.parent.GetComponentInChildren<EchoEffectFactory>();
         _audioSource = GetComponentInChildren<AudioSource>();
     }
     
+    private bool HasAudioManager(){
+        if(audioMgr) return audioMgr;
+        audioMgr = FindAnyObjectByType<AudioManager>();
+        return audioMgr != null;
+    }
+
+    public void PlaySonarSound(){
+        if(HasAudioManager()) audioMgr.PlayShot(snapEcho.soundEffect);
+    }
+
     public void CastSnapEchoEffect()
     {
         _echoEffectFactory.CreateAndCastEchoEffect(transform.position, snapEcho);
-        _audioSource.PlayOneShot(snapEcho.soundEffect);
+        // audio played through animation events
     }
 
     public void CastFootStepEchoEffect() {
         _echoEffectFactory.CreateAndCastEchoEffect(transform.position, footStepEcho);
-        _audioSource.PlayOneShot(snapEcho.soundEffect);
+        _audioSource.PlayOneShot(footStepEcho.soundEffect);
     }
 }
