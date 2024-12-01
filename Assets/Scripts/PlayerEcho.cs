7c8026f38abe2167808778add4a3e90a4ca44861
Xenoneqq <realfatboimlg@gmail.com>
2024-12-01 13:00:04 +0100

    Added sonar sound

--- Assets/Scripts/PlayerEcho.cs
+++ Assets/Scripts/PlayerEcho.cs
@@ -6,11 +6,25 @@ public class PlayerEcho : MonoBehaviour
     private EchoEffectFactory _echoEffectFactory;
     [SerializeField] private Echo footStepEcho;
     [SerializeField] private Echo snapEcho;
+    [SerializeField] private AudioClip snapEchoSound;
+    private AudioManager audioMgr;
+
     private void Start()
     {
+        HasAudioManager();
         _echoEffectFactory = transform.parent.GetComponentInChildren<EchoEffectFactory>();
     }
     
+    private bool HasAudioManager(){
+        if(audioMgr) return audioMgr;
+        audioMgr = FindAnyObjectByType<AudioManager>();
+        return audioMgr != null;
+    }
+
+    public void PlaySonarSound(){
+        if(HasAudioManager()) audioMgr.PlayShot(snapEchoSound);
+    }
+
     public void CastSnapEchoEffect()
     {
         _echoEffectFactory.CreateAndCastEchoEffect(transform.position, snapEcho);
